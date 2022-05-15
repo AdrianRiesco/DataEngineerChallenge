fb62a2339a6875cfb77f1825f398dd76707fa1ae
AdrianRiesco <adrian.riesco.valbuena@gmail.com>
2022-05-15 00:49:42 -0400

    Forms added

--- flask/forms.py
+++ flask/forms.py
@@ -1,7 +1,7 @@
 # Import libraries
 from flask_wtf import FlaskForm
 from wtforms import Form, StringField, PasswordField, RadioField, SubmitField, validators
-from wtforms.validators import DataRequired, InputRequired, EqualTo, Email
+from wtforms.validators import DataRequired, InputRequired, EqualTo, Email, Length, Regexp
 from flask_wtf.file import FileField, FileAllowed, FileRequired
 
 # Class for the login form
@@ -13,7 +13,9 @@ class LoginForm(FlaskForm):
 # Class for the signin form
 class RegisterForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired()])
-    username = StringField('Username', validators=[DataRequired()])
+    username = StringField('Username (will be used for log in)', validators=[DataRequired(),
+        Length(min=3, max=30, message='Username must have between 3 and 30 numbers and/or letters, and start with a letter.'),
+        Regexp('^[A-Za-z]+[0-9A-Za-z]*$', message='Username must have between 3 and 30 numbers and/or letters, and start with a letter.')])
     email = StringField('Email address', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[InputRequired(), EqualTo('confirm_password', message='Passwords must match')])
     confirm_password  = PasswordField('Repeat password')
@@ -23,3 +25,8 @@ class UploadForm(FlaskForm):
     file = FileField('Choose an image', validators=[FileRequired(), FileAllowed(['jpg', 'jpeg', 'png'], 'Wrong file format!')])
     visibility = RadioField('Visibility', choices=[('public','Public'),('private','Private')], default='public')
     send = SubmitField('Upload')
+
+class DeleteForm(FlaskForm):
+    filename = StringField('Filename', validators=[DataRequired()])
+    visibility = StringField('Visibility', validators=[DataRequired()])
+    send = SubmitField('Remove')
